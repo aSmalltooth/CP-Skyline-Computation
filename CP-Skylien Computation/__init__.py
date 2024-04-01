1b84905536ecce88ee8ab038e131208809ca439c
aSmalltooth <114995536+aSmalltooth@users.noreply.github.com>
2024-04-01 17:59:00 +0800

    Update __init__.py

--- CP-Skylien Computation/__init__.py
+++ CP-Skylien Computation/__init__.py
@@ -1,12 +1,1 @@
-import random
-
-num_lines =10000 # 选取的行数 3
-
-with open('data.txt', 'r') as f:
-    lines = f.readlines()
-
-selected_lines = random.sample(lines, num_lines)
-
-with open('10k_data.txt', 'w') as f:
-    f.writelines(selected_lines)
 
